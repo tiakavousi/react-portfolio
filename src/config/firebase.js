43450de71fdd71a3ebed2f3331bb105b1fba7951
Tayebe kavousi <tayebekavousi68@gmail.com>
2025-04-10 13:14:51 +0800

    edit config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -11,6 +11,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+console.log("API KEY:", process.env.REACT_APP_FIREBASE_API_KEY);
 
 
 const app = initializeApp(firebaseConfig);
