c1e35acd904e6567e43814f8abe9ddba0e881f48
Tayebe kavousi <tayebekavousi68@gmail.com>
2025-04-10 12:48:40 +0800

    edit config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,19 +4,17 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEnS-q195KbDlha41lVCZ-e54TgNLRpV0",
-  authDomain: "ecom-1001a.firebaseapp.com",
-  projectId: "ecom-1001a",
-  storageBucket: "ecom-1001a.appspot.com",
-  messagingSenderId: "59567538058",
-  appId: "1:59567538058:web:e7edd490ddcba70e7bd273",
-  measurementId: "G-KZ77YGZ3ED"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID, // optional
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore, Auth, and Storage
 const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
